5f6cdde1299530a50232168ff89bd00aa62226e0
Jedro1270 <jedro1270@gmail.com>
2022-10-14 09:51:09 +0800

    feat: finalize main menu

---
+++ Assets/_InternalAssets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int secondsToFadeOut = 3;
+    public AudioSource audio;
+
+    IEnumerator FadeAudioAndLoadScene()
+    {
+        while (audio.volume > 0.01f)
+        {
+            audio.volume -= Time.deltaTime / secondsToFadeOut;
+            yield return null;
+        }
+
+        audio.volume = 0;
+        audio.Stop();
+
+        SceneManager.LoadScene("Battlefield");    }
+
+    public void StartGame()
+    {
+        StartCoroutine(FadeAudioAndLoadScene());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
