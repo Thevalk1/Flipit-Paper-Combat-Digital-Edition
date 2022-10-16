020b13f01578dbcd61e6fcc156cbb95a437cd928
Jedro1270 <jedro1270@gmail.com>
2022-10-17 07:55:10 +0800

    feat: improve coin toss behavior

--- Assets/_InternalAssets/Scripts/CoinBehavior.cs
+++ Assets/_InternalAssets/Scripts/CoinBehavior.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject coin;
     public Rigidbody rb;
 
-    void Start()
-    {
+    private float speed;
+    private bool tossedCoin = false;
+
+    void Start() {
         TossCoin();
+    }
+
+    void Update () {
+        if (tossedCoin) {
+            StartCoroutine(GetResult());
+        }
     }
 
     public void TossCoin()
     {
+        tossedCoin = true;
+
         int jumpForce = Random.Range(50, 100);
         rb.AddForce(0, jumpForce, 10);
         int torqx = Random.Range(50, 100);
         int torqy = Random.Range(50, 100);
         rb.AddTorque(torqx, 0, torqy);
+    }
+
+    private IEnumerator GetResult()
+    {
+        while (!Mathf.Approximately(rb.velocity.magnitude, 0))
+        {
+            yield return null;
+        }
+
+        rb.velocity = Vector3.zero;
+
+        tossedCoin = false;
+
+        Debug.Log("Need to get face up value now");
+
+        // TODO: Proceed to movement phase after getting winner in coin toss
     }
 }
