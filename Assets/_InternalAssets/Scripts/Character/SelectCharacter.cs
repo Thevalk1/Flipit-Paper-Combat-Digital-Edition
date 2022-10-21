536db0ad2125233677edb9d40537db8fdb2b635c
Jedro1270 <jedro1270@gmail.com>
2022-10-21 09:33:15 +0800

    feat: add outline on characters when selecting

--- Assets/_InternalAssets/Scripts/Character/SelectCharacter.cs
+++ Assets/_InternalAssets/Scripts/Character/SelectCharacter.cs
@@ -8,7 +8,34 @@ public class SelectCharacter : MonoBehaviour
 
     void OnMouseOver()
     {
-        // TODO: Glow character
+        if (
+            (
+                GameManager.Instance.State == GameState.Player1MovementTurn
+                && GameManager.Instance.player1Faction == faction
+            )
+            || (
+                GameManager.Instance.State == GameState.Player2MovementTurn
+                && GameManager.Instance.player2Faction == faction
+            )
+            || (
+                GameManager.Instance.State == GameState.Player1ShootingTurn
+                && GameManager.Instance.player1Faction == faction
+            )
+            || (
+                GameManager.Instance.State == GameState.Player2ShootingTurn
+                && GameManager.Instance.player2Faction == faction
+            )
+        )
+        {
+            if (GetComponent<Outline>() == null)
+            {
+                var outline = gameObject.AddComponent<Outline>();
+
+                outline.OutlineMode = Outline.Mode.OutlineAll;
+                outline.OutlineColor = Color.yellow;
+                outline.OutlineWidth = 5f;
+            }
+        }
 
         if (Input.GetMouseButtonDown(1))
         {
@@ -38,6 +65,14 @@ public class SelectCharacter : MonoBehaviour
             {
                 GameManager.Instance.StartCharacterShooting(gameObject);
             }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (GetComponent<Outline>())
+        {
+            Destroy(GetComponent<Outline>());
         }
     }
 }
